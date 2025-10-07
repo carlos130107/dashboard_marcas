1e9ef0432764b0788e1165abb19ce481cfc3d615
Carlos Henrique Bueno <carlos.henrique@SOS.corp>
2025-10-07 14:03:41 -0300

    Atualiza dashboard Streamlit com novos dados e ajustes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import altair as alt
 # CONFIGURAÇÕES DA PÁGINA
 st.set_page_config(
     page_title="Análise das Marcas",
-    page_icon= "📈",
+    page_icon= "grafic.png",
     layout="wide")
 
 # CSS PARA DARK MODE COMPLETO
